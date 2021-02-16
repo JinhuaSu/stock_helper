02361d6023cf50ff08d45e9630a6b3b951df66dd
JinhuaSu <944866518@qq.com>
2021-02-16 02:42:15 +0000

    fix: a bug and low frequency

--- src/test/check_online_workers.py
+++ src/test/check_online_workers.py
@@ -5,6 +5,7 @@ import time
 import os
 import sys
 
+
 sys.path.append("../")
 from toolBox import EmailBot
 
@@ -12,20 +13,28 @@ os.chdir("../")
 emailbot = EmailBot("../data/settings.json")
 
 config_path_list = ["../data/pool1.json", "../data/pool2.json"]
+
+count = 0
 flag = 0
 while True:
-    time.sleep(60)
+    time.sleep(1)
+    count += 1
     for config_path in config_path_list:
         config_dict = json.load(open(config_path))
-        url = "https://api.f2pool.com/{type}/{address}".format(
-            type=config_dict["type"], address=config_dict["address"]
-        )
-        resp = requests.get(url)
-        if resp.status_code == 200:
-            data_dict = json.loads(resp.text)
-            if data_dict["worker_length_online"] < config_dict["number"]:
-                flag += 1
-                content = "pool for {type} has some bot offline pleace check"
-                emailbot.sendOne({"content": content})
+        if count % config_dict["check_per_seconds"] == 0:
+            url = "https://api.f2pool.com/{type}/{address}".format(
+                type=config_dict["type"], address=config_dict["address"]
+            )
+            resp = requests.get(url)
+            if resp.status_code == 200:
+                data_dict = json.loads(resp.text)
+                if data_dict["worker_length_online"] < config_dict["number"]:
+                    flag += 1
+                    content = (
+                        "pool for {type} has some bot offline pleace check".format(
+                            config_dict["type"]
+                        )
+                    )
+                    emailbot.sendOne({"content": content})
     if flag >= 6:
         break
